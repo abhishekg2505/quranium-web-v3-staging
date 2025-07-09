a23360a3aa8bfe1d4ea8be785fba156c442d82f8
abhishekg2505 <abhishekg2505@gmail.com>
2025-07-09 16:47:46 +0530

    create community page

--- src/components/community/PromoteQuranium.tsx
+++ src/components/community/PromoteQuranium.tsx
@@ -168,14 +168,14 @@ const PromoteQuranium: React.FC = () => {
           {/* Navigation Arrows */}
           <div className="flex justify-center gap-6 mt-6 md:mt-0 md:block">
             {/* Left Arrow */}
-            <div className="swiper-prev md:absolute md:-left-7 md:top-1/2 md:transform md:-translate-y-1/2 z-10 bg-[#3C3747] hover:bg-white/20 p-2 rounded-full cursor-pointer">
+            {/* <div className="swiper-prev md:absolute md:-left-7 md:top-1/2 md:transform md:-translate-y-1/2 z-10 bg-[#3C3747] hover:bg-white/20 p-2 rounded-full cursor-pointer">
               <MoveLeft className="text-white" />
-            </div>
+            </div> */}
 
             {/* Right Arrow */}
-            <div className="swiper-next md:absolute md:-right-7 md:top-1/2 md:transform md:-translate-y-1/2 z-10 bg-[#3C3747] hover:bg-white/20 p-2 rounded-full cursor-pointer">
+            {/* <div className="swiper-next md:absolute md:-right-7 md:top-1/2 md:transform md:-translate-y-1/2 z-10 bg-[#3C3747] hover:bg-white/20 p-2 rounded-full cursor-pointer">
               <MoveRight className="text-white" />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
