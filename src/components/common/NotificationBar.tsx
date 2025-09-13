65b2e510fbe9d276b5bb38ef914f9e1108acbc14
abhishekg2505 <abhishekg2505@gmail.com>
2025-09-13 19:43:53 +0530

    update announcemnt bar

--- src/components/common/NotificationBar.tsx
+++ src/components/common/NotificationBar.tsx
@@ -8,8 +8,8 @@ const NotificationBar = () => {
     <div className="mb-2 relative">
       <div className="px-4 before:absolute before:inset-0 before:bg-[url('/images/homepage/nb-small.webp')] md:before:bg-[url('/images/homepage/nb-large.webp')] before:bg-cover before:bg-center before:bg-no-repeat before:-z-10">
         <div className="relative">
-          <div className="relative z-10  mx-auto  flex flex-row items-center justify-center gap-5 py-[5px]">
-            <h5 className="text-white text-p2 md:text-h6 font-normal md:font-medium leading-tight">
+          <div className="relative z-10  mx-auto  flex flex-row items-center justify-center gap-5 py-[15px]">
+            <h5 className="text-white text-p2 md:text-h6 font-medium leading-tight">
               <span className="text-[#D0FF04]">$QRN</span> Public Pre-Sale is now live on Republic -
               Ends Soon.
             </h5>
