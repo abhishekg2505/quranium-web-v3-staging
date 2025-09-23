d95f5d5add77ba71ce390ab2df717c2ec145af8d
abhishekg2505 <abhishekg2505@gmail.com>
2025-09-23 13:42:39 +0530

    spelling correction

--- src/components/common/NotificationBar.tsx
+++ src/components/common/NotificationBar.tsx
@@ -10,20 +10,16 @@ const NotificationBar = () => {
         <div className="relative">
           <div className="relative z-10  mx-auto  flex flex-row items-center justify-center gap-5 py-[15px]">
             <h5 className="text-white text-p2 md:text-h6 font-medium leading-tight">
-              <span className="text-[#00FFA5]"></span> Meet Quranium at CV Summit Zurich — 23 to 24
-              September, 2025
+              <span className="text-[#00FFA5]">$QRN</span> Public Pre-Sale is now live on Republic -
+              Ends Soon.
             </h5>
 
             <div className="">
-              <Link
-                href="https://luma.com/CVSummit2025?coupon=QURANIUM_FREE"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <Link href="https://republic.com/quranium" target="_blank" rel="noopener noreferrer">
                 <Button className="group relative overflow-hidden text-[#000] hover:text-[#000] bg-[#00FFA5]  border-[#00FFA5] hover:bg-transparent">
                   <span className="absolute inset-0 z-0 bg-[#fff] transform -translate-x-[101%] transition-transform duration-500 ease-in-out group-hover:translate-x-0"></span>
                   <span className="relative z-10 flex items-center text-[#000] group-hover:text-[#000] transition-colors duration-500">
-                    Register Today
+                    Invest Now
                   </span>
                 </Button>
               </Link>
