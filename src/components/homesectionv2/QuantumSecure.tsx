1b7698d6d0e153657046eb856c3d2c1a0db1b483
abhishekg2505 <abhishekg2505@gmail.com>
2025-10-15 19:07:23 +0530

    add homev2

--- src/components/homesectionv2/QuantumSecure.tsx
+++ src/components/homesectionv2/QuantumSecure.tsx
@@ -82,11 +82,19 @@ export default function QuantumSecure() {
 
       {/* Animated text section */}
       <div className="relative max-w-7xl mx-auto px-[16px] md:px-[60px] mt-[100px]">
-        <h2 className="text-h4 md:text-h3 font-montserrat font-semibold text-center flex justify-center items-center gap-2">
+        <h2 className="text-h4 md:text-h3 font-montserrat font-semibold text-center hidden md:flex justify-center items-center gap-2">
           Quantum Secure{" "}
           <span className="text-[#C994FE] h-[1.2em] flex items-center overflow-hidden relative">
             <span key={index} className="block animate-slide-up text-[#C994FE] overflow-visible">
               {words[index]}
+            </span>
+          </span>
+        </h2>
+        <h2 className="text-h4 md:text-h3 font-montserrat font-semibold text-center flex md:hidden justify-center items-center gap-2">
+          Quantum Secure{" "}
+          <span className="text-[#C994FE] h-[1.5em] flex items-center overflow-hidden relative">
+            <span className="block animate-slide-up text-[#C994FE] overflow-visible">
+              Transaction
             </span>
           </span>
         </h2>
